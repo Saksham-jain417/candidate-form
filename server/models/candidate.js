96f91e5a989b40f5ac48b4775bea15a28a8ee64e
Saksham-jain417 <hsr.saksham@gmail.com>
2025-06-26 11:16:15 +0530

    Fix: Updated candidate schema to support array of project objects

--- server/models/candidate.js
+++ server/models/candidate.js
@@ -1,15 +1,54 @@
 const mongoose = require('mongoose');
 
 const candidateSchema = new mongoose.Schema({
-    name: String,
+    fullName: String,
     email: String,
     phone: String,
     portfolio: String,
-    experience: String,
-    projects: String,
-    linkedin: String,
+    trigram: { type: String, required: true, unique: true },
+    summary: String,
+    education: {
+        secondary: {
+            school: String,
+            board: String,
+            year: String,
+        },
+        seniorSecondary: {
+            school: String,
+            board: String,
+            year: String,
+        },
+        graduation: {
+            degree: String,
+            institution: String,
+            year: String,
+        },
+        postGraduation: {
+            degree: String,
+            institution: String,
+            year: String,
+        }
+    },
+    experiences: [
+        {
+            designation: String,
+            duration: String,
+            company: String,
+            summary: String,
+            projects: [String],
+        }
+    ],
+    projects: [
+        {
+            title: String,
+            technologies: String,
+            summary: String,
+            link: String,
+        }
+    ],
     achievements: [String],
-    certifications: String
-}, { timestamps: true });
+    linkedin: String,
+    github: String
+});
 
 module.exports = mongoose.model('Candidate', candidateSchema);
